35262e589f8a33b033f73328e4139abb06aa72f0
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-07 13:41:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+/*
+*  This is the function main() that prints the string and returns 0.
+*/
 int main(void)
 {
-	/*
-	 *  This is the function main() that prints the string and returns 0.
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
