73aa380da9e9e1d7886aac14c0dc7aa22bc7a284
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-07 14:19:53 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the string "with proper grammar, but the outcome is 
+ * main - Prints the string "with proper grammar, but the outcome is
  * a piece of art,", followed by a new line.
  *
  *
