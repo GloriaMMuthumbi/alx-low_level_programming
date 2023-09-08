2cbe3c62a26bbdb0bd903222d14c3ded8c12ca66
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-08 06:40:22 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - this main function prints the alphabet
+ * in lowercase without using printf.it uses for
+ * for loop and putchar only appears twice.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
