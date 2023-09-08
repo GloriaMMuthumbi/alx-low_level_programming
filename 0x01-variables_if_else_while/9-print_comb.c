f7fdac89aaec434396d745235d35debcb0021353
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-08 11:57:48 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - this function prints all possible
+ * combinations of single-digit numbers. They are
+ * seperated by , followed by a space. they are in
+ * ascending order. no use of type char.
+ *
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
