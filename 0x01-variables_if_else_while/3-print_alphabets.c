f697d93b76aca5247a7d5dd135a27fcbe24796c2
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-08 06:01:57 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - this main function prints the alphabet
+ * in lowercase followed by the alphabet in uppercase.
+ * it does not use printf. it uses for loop and
+ * putchar only appears thrice.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char uch, lch;
+
+	for (uch = 'A'; uch <= 'Z'; uch++)
+	{
+		for (lch = 'a'; lch <= 'z'; lch++)
+		{
+			putchar(lch);
+		}
+		putchar(uch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
