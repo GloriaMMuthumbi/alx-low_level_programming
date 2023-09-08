39edb3101f48fc65a5e0f790f73be826ab6735c2
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-08 13:02:49 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints all possible
+ * combinations of two two-digit numbers in ascending
+ * order, seperated by a comma and a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num1, num2;
+	int first_combination = 1;
+
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = num1; num2 <= 99; num2++)
+		{
+			if (!first_combination)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			first_combination = 0;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
