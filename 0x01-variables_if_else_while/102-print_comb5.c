1ac69da12a1606e6d535a4c69b43f4d03044a279
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-08 13:05:34 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,30 +10,29 @@
 int main(void)
 {
 	int num1, num2;
-	int first_combination = 1;
+    int first_combination = 1;
 
-	for (num1 = 0; num1 <= 99; num1++)
-	{
-		for (num2 = num1; num2 <= 99; num2++)
-		{
-			if (!first_combination)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+    for (num1 = 0; num1 <= 99; num1++)
+    {
+        for (num2 = num1 + 1; num2 <= 99; num2++)
+        {
+            if (!first_combination)
+            {
+                putchar(',');
+                putchar(' ');
+            }
 
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
+            putchar((num1 / 10) + '0');
+            putchar((num1 % 10) + '0');
+            putchar(' ');
+            putchar((num2 / 10) + '0');
+            putchar((num2 % 10) + '0');
 
-			first_combination = 0;
-		}
-	}
+            first_combination = 0;
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
-
