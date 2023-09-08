a4a42b02c60e6eccf489e5d0e36cf01838f66afb
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-08 05:19:28 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <strdlib.h>
+
+/**
+ * main - this main function prints the alphabet
+ * in lowercase without using printf.it uses for
+ * for loop and putchar only appears twice.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch='a'; ch<='z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
