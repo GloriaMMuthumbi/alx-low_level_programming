5628904bddb5023763d8ccc1714d98092a35533d
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-12 16:17:55 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -20,12 +20,13 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n < 0)
-        {
-                _putchar('-');
-                return (-1);
-        }else if (n == 0)
-        {
-               _putchar(48);
-        }
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48)
+	}
 	return (0);
 }
