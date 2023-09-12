7f989474c98ff445b52a9220eb747bbd70d5600c
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-12 11:38:09 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,21 +3,29 @@
 using namespace std;
 
 /*
- * main - this function prints "_putchar" without using
+ * _putchar funtion - this function prints "_putchar" without using
  * printf or puts followed by a new line.
+ *
+ * main - this function uses the _putchar() to print the input
  *
  * Return: Always 0.
  */
 
-int main(void)
+int _putchar(char * word)
 {
-	string word[] = "_putchar";
 	int i;
-
+	
 	for(i = 0; i < word.length() - 1; i++)
 		putchar(word[i]);
-
+	
 	putchar('\n');
+}
+
+
+int main(void)
+{
+	char * word = "_putchar";
+	_putchar(word);
 
 	return 0;
 }
