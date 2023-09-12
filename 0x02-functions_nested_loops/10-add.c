e23230020ac35e7d156435aca2059e720f42e31f
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-12 16:53:42 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integrers and returns the sum
+ *
+ * @x: number to be added to y
+ *
+ * @y: number to be added to x
+ *
+ * Return: Always 0.
+ *
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
