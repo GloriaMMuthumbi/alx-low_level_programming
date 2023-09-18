450fd1b5b5ad0d0e51f1742633734a5d326dfaeb
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-18 05:12:24 -0700

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,13 +7,11 @@
  */
 int _strlen(char *s)
 {
-    int count = 0;
-
-    while (*s != '\0') {
-        count++;
-        s++;
-    }
-
-    return (count);
+	int count = 0;
+	
+	while (*s != '\0') {
+		count++;
+		s++;
+	}
+	return (count);
 }
-
