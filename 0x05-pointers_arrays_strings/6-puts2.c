8ec513dc2cf668b279d6028ae7ae0b9a3e0b87da
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-18 07:56:00 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - it prints every other character of a string
+ * starting with the first character followed by a new
+ * line
+ * @str: the string to be printed
+ *
+ * Return: return nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
