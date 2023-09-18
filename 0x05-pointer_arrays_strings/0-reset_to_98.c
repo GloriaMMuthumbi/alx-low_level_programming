d37203a000253bd0446a09fb26560a0e737b6e34
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-18 04:08:44 -0700

    98 Battery st.

---
+++ 0x05-pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * rester_to_98 - it takes a pointer to an int as a parameter
+ * and updates the value it points to 98
+ * @*n: the pointer being updated
+ *
+ * Returns: returns nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointer_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - this is a header file for the prototypes
+ */
+
+void reset_to_98(int *n);
+
+#endif /*MAIN_H*/
