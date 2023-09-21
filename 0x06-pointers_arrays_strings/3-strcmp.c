f241a9210298403b7bc9b13bff2ae1189805adf0
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-21 06:57:59 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcmp - this function compares to string
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: returns 0 if value is 0,
+ * 1 if non-matching first character in s1 has a larger
+ * value than the one in s2 and -1 if non-matching
+ * first character in s1 has a smaller value
+ * than the one in s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			if ((int)(*s1) < (int)(*s2))
+				return (-1);
+			else if ((int)(*s1) > (int)(*s2))
+                                return (1);
+			else
+				return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (0);
+}
