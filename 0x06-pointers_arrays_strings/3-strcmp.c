bc5b1c059d1c8c978e3101e4dd5815f2d1f2e233
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-21 09:57:32 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,19 +12,30 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	int i = 0, diff = 0;
+
+	while (1)
 	{
-		if (*s1 != *s2)
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
 		{
-			if ((int)(*s1) < (int)(*s2))
-				return (-15);
-			else if ((int)(*s1) > (int)(*s2))
-				return (15);
-			else
-				return (0);
+			diff = s2[i];
+			break;
 		}
-		s1++;
-		s2++;
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+
 	}
-	return (0);
+	return (diff);
 }
