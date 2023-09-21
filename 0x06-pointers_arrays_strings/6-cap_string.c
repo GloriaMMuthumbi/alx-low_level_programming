a3328b524d9e2a2413f39adeca700cc4ce7d649f
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-21 09:19:58 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,12 +9,21 @@
 char *cap_string(char *string)
 {
 	int i = 0;
+	int j;
+	char characters[] = " \t\n,;.!?\"(){}";
 
 	while (string[i] != '\0')
 	{
-		if (string[i] >= 'A' && string[i] <= 'Z')
-			string[i] += 32;
+		if ( string[0] >= 'a' && string[0] <= 'z')
+			string[0] -= 32;
+
+		if (string[i] == ' ' || string[i] == '\t' || string[i] == '.' || string[i] == '\n' || string[i] == ',' || string[i] == '?' || string[i] == '!' || string[i] == '(' || string[i] == ')' || string[i] == '{' || string[i] == '}' || string[i] == '"' || string[i] == ';')
+		{
+			if (string[i + 1] >= 'a' && string[i + 1] <= 'z')
+				string[i + 1] -= 32;
+		}
 		i++;
 	}
+
 	return (string);
 }
