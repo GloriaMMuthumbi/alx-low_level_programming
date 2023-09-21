bde6d84ace2edc4722bc0e2e37e6ac73346ec992
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-21 08:39:08 -0700

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,16 +8,13 @@
  */
 char *string_toupper(char *string)
 {
-	char *pointer = string;
 	int i = 0;
 
-	while (*pointer != '\0')
+	while (string[i] != '\0')
 	{
-		if (*pointer >= 'a' && *pointer <= 'z')
+		if (string[i] >= 'a' && string[i] <= 'z')
 			string[i] -= 32;
-
 		i++;
 	}
-
 	return (string);
 }
