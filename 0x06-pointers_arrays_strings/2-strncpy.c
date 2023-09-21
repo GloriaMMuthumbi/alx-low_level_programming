3312d6cde695822dcb178b9220a49fd4abe47906
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-21 06:15:15 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,11 +14,6 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *dest_address = dest;
 
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-
 	while (*src != '\0' && n > 0)
 	{
 		*dest = *src;
@@ -27,7 +22,13 @@ char *_strncpy(char *dest, char *src, int n)
 		n--;
 	}
 
-	*dest = '\0';
+
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
 
 	return (dest_address);
 }
