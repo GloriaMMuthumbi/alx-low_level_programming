dfdf3de40550b9690b79d5e0ffc809c7f689a62b
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-27 05:45:58 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This folder is for the tasks in 0x08. C - Recursion
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - this is a header file for the prototypes
+ */
+
+int _putchar(char l);
+void _puts_recursion(char *s);
+
+#endif /*MAIN_H*/
