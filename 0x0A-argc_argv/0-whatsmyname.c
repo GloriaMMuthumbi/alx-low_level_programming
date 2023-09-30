c54a10dc6171a897cac2caa230f87e3cd79d3c73
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-09-29 17:46:32 -0700

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,16 @@
 /**
  * main - prints name of program followed
  * by a new line
+ * @argc: integer represents number of command line
+ * arguments
+ * @argv: array containing the command line arguments
+ *
+ * Return: returns 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	if(argc > 0 && argv[0])
+	if (argc > 0 && argv[0])
 		printf("%s\n", argv[0]);
 
-	return 0;
+	return (0);
 }
