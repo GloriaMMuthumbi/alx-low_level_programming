cc35f638494ef70d597da31a4f3a44dec9ec698e
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-10-19 15:04:57 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t c = 1;
-	int length = h->len;
+	size_t c = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %s\n", length, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
 		c++;
