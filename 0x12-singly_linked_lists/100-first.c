911f42eff355973b0c4ef52761216a814a6d45b6
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-10-19 16:13:02 -0700

    The hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_before(void) __attribute__((constructor));
+/**
+ * print_before - prints the text
+ * Return: returns nothing
+ */
+void print_before(void)
+{
+	printf("You are beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
