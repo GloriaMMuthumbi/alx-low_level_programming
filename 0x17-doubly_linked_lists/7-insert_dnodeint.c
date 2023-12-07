f711e19507f69305fbd57835b14d9201bb9a602d
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-12-07 13:40:14 -0800

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - inserts a new node in list at index 
+ * insert_dnodeint_at_index - inserts a new node in list at index
  * @h: points to pointer of head of list
  * @idx: index of new node inserted
  * @n: value of new node
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = temp;
 	new_node->next = temp->next;
-	temp->next ->prev = new_node;
+	temp->next->prev = new_node;
 	temp->next = new_node;
 
 	return (new_node);
