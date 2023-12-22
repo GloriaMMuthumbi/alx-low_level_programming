ca52220c3ba59cabcdd743acaa801743531348b8
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-12-22 01:22:10 -0800

    4. >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table
+ * @key: the key
+ * Return: returns the value associated with element, otherwise
+ * NULL if key is not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current_node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	current_node = ht->array[index];
+
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+	return (NULL);
+}
