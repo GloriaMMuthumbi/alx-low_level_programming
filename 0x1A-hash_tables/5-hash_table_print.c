f33140a1b3ef8317386ea7408505676563aa113a
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-12-22 01:35:19 -0800

    5. >>> print(ht)

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,6 +1,8 @@
 #include "hash_tables.h"
 /**
- *
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ * Return: returns nothing
  */
 void hash_table_print(const hash_table_t *ht)
 {
