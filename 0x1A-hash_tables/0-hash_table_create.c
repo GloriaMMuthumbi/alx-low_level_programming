84a6dd218797176d8e04ed340290c95d537d371a
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-12-22 00:40:25 -0800

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,9 @@
 #include "hash_tables.h"
 /**
- *
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: returns a pointer to the newly created hash table.
+ * otherwise return NULL.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
