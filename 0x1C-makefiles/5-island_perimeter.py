7223acac76d9cc4d87833b94cf96e148ce190c33
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-02-28 23:51:01 -0800

    5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/urs/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island in the grid
+
+    Args:
+        grid (List[List[int]]): list of lists representing the island grid
+
+    Returns:
+        int: the preimeter of the island
+    """
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter +=4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
